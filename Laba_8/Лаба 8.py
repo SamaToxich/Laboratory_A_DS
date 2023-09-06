d3d0fe875c379115ad48c386aafb64001564c7be
Георгий Лисин <125073984+SamaToxich@users.noreply.github.com>
2023-09-06 16:29:28 +0400

    Add files via upload

--- Laba_8/Лаба 8.py
+++ Laba_8/Лаба 8.py
@@ -16,118 +16,120 @@ class selection:
 
         self.main = main
         self.main_label = Label(main, text='Люди у которых возраст превышает 30 лет не'
-                                           'допускаются к участию в соревновании.\nНужно вывести составы команд и их '
+                                           'допускаются к участию в соревновании.\nНужно вывести составы команд, их '
                                            'параметры и команду с наибольшим суммарным рейтингом игроков.\n')
 
-        self.label1 = Label(text='\nВведите название таблицы:')
-        self.entry1 = ttk.Entry(width=30, justify='center')
+        self.label = Label(text='\nВведите кол-во игроков в турнире:')
+        self.entry = ttk.Entry(width=30, justify='center')
 
         self.main_button = ttk.Button(text='Вывести', command=self.result)
 
         self.main_label.pack()
-        self.label1.pack()
-        self.entry1.pack()
+        self.label.pack()
+        self.entry.pack()
         self.main_button.pack(expand=True)
-
-    def parameters(self, x):
-        self.a, self.b = x
-        self.a1, self.b1 = r.randint(1, 10), r.randint(1, 10)
-        self.mp[self.a, self.b] = self.participants_parameters[self.a], self.participants_parameters[self.b]
-        self.rating.append(self.a1 + self.b1)
 
     def result(self):
         self.m = []
         self.mp = {}
+        self.string = ''
         self.rating = []
-        self.participants = ['Ж1', 'Ж2', 'Ж3', 'М1', 'М2', 'М3', 'М4', 'М5']
-        self.participants_parameters = {'Ж1': 19, 'Ж2': 27, 'Ж3': 35, 'М1': 24, 'М2': 38, 'М3': 21, 'М4': 18, 'М5': 43}
+        self.player = []
+        self.table = []
+        self.player_parameters = {}
 
-        self.conditions = True
+        try:
+            self.conditions = True
+            self.point = int(self.entry.get())
 
-        if self.conditions:
-            self.calculations()
+            if self.point < 4:
+                messagebox.showwarning(title='Ошибка', message='Минимум четыре игрока.')
+                self.conditions = False
 
-            if self.first_click:
-                self.result_window()
-                self.first_click = False
-            else:
-                self.purchases_window.destroy()
-                self.result_window()
+            if self.conditions:
+                self.calculations()
+
+                if self.first_click:
+                    self.result_window()
+                    self.first_click = False
+                else:
+                    self.purchases_window.destroy()
+                    self.result_window()
+        except ValueError:
+            messagebox.showwarning(title='Ошибка', message='Введите число.')
+
+    def players(self, x):
+        cntplayers = 1
+
+        for _ in range(x):
+            self.player.append(f'{r.choice("МЖ")}{cntplayers}')
+            cntplayers += 1
+
+        for j in self.player:
+            self.player_parameters[j] = r.randint(18, 35)
+
+        for i in self.player_parameters.items():
+            a, b = i
+            self.string = f'{self.string}, {a} {b}'
+
+    def parameters(self, x):
+        a, b = x
+        a1, b1 = r.randint(1, 50), r.randint(1, 50)
+        self.mp[a, b] = self.player_parameters[a], self.player_parameters[b]
+        self.rating.append(a1 + b1)
 
     def calculations(self):
-        self.y = 0
-        for i, n in enumerate(self.participants_parameters.items()):
-            self.a, self.b = n
-            if int(self.b) > 30:
-                i -= self.y
-                self.y += 1
-                self.participants.pop(i)
-        for j in itertools.combinations(self.participants, 2):
-            self.parameters(j)
-        # Заготовки для таблицы
-        self.table = []
-        # Разделение списка на составляющие
-        self.table.append(['№ команды', 'Игрок № 1', 'Игрок № 2', 'Возраст № 1', 'Возраст № 2', 'Рейтинг'])
-        for j, n in enumerate(self.mp.items()):
-            self.p1, self.p2 = (str(n[0])[1:-1]).split(', ')
-            self.r1, self.r2 = (str(n[1])[1:-1]).split(', ')
-            self.table.append([j + 1, self.p1, self.p2, self.r1, self.r2, self.rating[j]])
+        y = 0
 
-        # Заготовки для таблицы
-        self.a = '¯' * 83
-        self.b = '_' * 83
-        self.c = '-' * 83
+        self.players(self.point)
+
+        for i, n in enumerate(self.player_parameters.items()):
+            a, b = n
+            if int(b) > 30:
+                i -= y
+                y += 1
+                self.player.pop(i)
+        for j in itertools.combinations(self.player, 2):
+            self.parameters(j)
+
+        self.table += ['Игроки подавшие заявку:', f'{self.string[2::]}', '', 'Составы команд:', f'|{"¯" * 83}|',
+                       '|{:^11}|{:^11}|{:^11}|{:^13}|{:^13}|{:^9}|'.format('№ команды', 'Игрок № 1', 'Игрок № 2',
+                                                                           'Возраст № 1', 'Возраст № 2', 'Рейтинг'),
+                       f'|{"-" * 83}|']
+
+        for j, n in enumerate(self.mp.items()):
+            p1, p2 = (str(n[0])[1:-1]).split(', ')
+            r1, r2 = (str(n[1])[1:-1]).split(', ')
+            self.table += ['|{:^23}|{:^18}|{:^18}|{:^21}|{:^24}|{:^15}|'.format(j + 1, p1, p2, r1, r2, self.rating[j])]
+
+        self.table += [f'|{"_" * 83}|', ' ', 'Команда с наивысшим рейтингом:', f'|{"¯" * 68}|',
+                       '|{:^11}|{:^11}|{:^13}|{:^13}|{:^9}|'.format('Игрок № 1', 'Игрок № 2', 'Возраст № 1',
+                                                                    'Возраст № 2', 'Рейтинг'), f'|{"-" * 68}|']
         # Разделение списка на составляющие
-        self.key = list(self.mp.items())[self.rating.index(max(self.rating))]
-        self.p1, self.p2 = (str(self.key[0])[1:-1]).split(', ')
-        self.r1, self.r2 = (str(self.key[1])[1:-1]).split(', ')
+        key = list(self.mp.items())[self.rating.index(max(self.rating))]
+        p1, p2 = (str(key[0])[1:-1]).split(', ')
+        r1, r2 = (str(key[1])[1:-1]).split(', ')
+        self.table += ['|{:^21}|{:^19}|{:^19}|{:^19}|{:^18}|'.format(p1, p2, r1, r2, max(self.rating)),
+                       f'|{"_" * 68}|']
 
     def result_window(self):
         self.purchases_window = Toplevel()
         self.purchases_window.title('Вывод')
         self.purchases_window.geometry('720x480')
 
-        self.label2 = Label(self.purchases_window, text=self.entry1.get())
-        self.label2.pack()
-        self.label3 = Label(self.purchases_window, text=f'|{self.a}|')
-        self.label3.pack()
-        self.label4 = Label(self.purchases_window, text='|{:^11}|{:^11}|{:^11}|{:^13}|{:^13}|{:^9}|'.format(
-            self.table[0][0], self.table[0][1], self.table[0][2], self.table[0][3],
-            self.table[0][4], self.table[0][5]))
-        self.label4.pack()
-        self.label5 = Label(self.purchases_window, text=f'|{self.c}|')
-        self.label5.pack()
-        for i in self.table[1::]:
-            self.label6 = Label(self.purchases_window, text='|{:^23}|{:^18}|{:^18}|{:^21}|{:^24}|{:^15}|'.format(
-                i[0], i[1], i[2], i[3], i[4], i[5]))
-            self.label6.pack()
-        self.label7 = Label(self.purchases_window, text=f'|{self.b}|')
-        self.label7.pack()
-        self.label8 = Label(self.purchases_window, text='\nКоманда с наивысшим рейтингом:')
-        self.label8.pack()
-        self.label9 = Label(self.purchases_window, text=f'|{"¯" * 68}|')
-        self.label9.pack()
-        self.label10 = Label(self.purchases_window,
-                             text='|{:^11}|{:^11}|{:^13}|{:^13}|{:^9}|'.format('Игрок № 1', 'Игрок № 2', 'Возраст № 1',
-                                                                               'Возраст № 2', 'Рейтинг'))
-        self.label10.pack()
-        self.label11 = Label(self.purchases_window, text=f'|{"-" * 68}|')
-        self.label11.pack()
+        self.purchases_list = Listbox(self.purchases_window)
+        self.purchases_list.pack(side='left', fill='both', expand=1)
 
-        key = list(self.mp.items())[self.rating.index(max(self.rating))]
-        p1, p2 = (str(key[0])[1:-1]).split(', ')
-        r1, r2 = (str(key[1])[1:-1]).split(', ')
+        for i in self.table:
+            self.purchases_list.insert('end', i)
 
-        self.label12 = Label(self.purchases_window,
-                             text='|{:^21}|{:^19}|{:^19}|{:^19}|{:^18}|'.format(p1, p2, r1, r2, max(self.rating)))
-        self.label12.pack()
-        self.label13 = Label(self.purchases_window, text=f'|{"_" * 68}|')
-        self.label13.pack()
+        self.scrollbar = Scrollbar(self.purchases_window, command=self.purchases_list.yview)
+        self.scrollbar.pack(side='right', fill='y')
 
 
 root = Tk()
 root.title('Лабораторная № 8')
-root.geometry('720x320')
+root.geometry('720x240')
 root.resizable(False, False)
 
 selection(root)
